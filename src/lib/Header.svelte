2cd917d0d4d5f4d620875e3a43b5d8b0c3bf2a92
B8N4S8N <79737258+B8N4S8N@users.noreply.github.com>
2023-03-11 19:20:19 +0000

    change  deploy button and url

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -7,11 +7,11 @@
 >
 	<button on:click class="flex items-center mb-4 md:mb-0 text-white/70">
 		<TvIcon />
-		<div class="text-2xl md:text-xl font-bold ml-2">watchthis.dev</div>
+		<div class="text-2xl md:text-xl font-bold ml-2">chill.opensourcerecovery.life</div>
 	</button>
 	<a
 		class="bg-white/20 hover:bg-white/50 shadow text-white hover:text-pink-600 font-medium rounded p-2 text-sm flex items-center"
-		href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2FStephDietz%2Fwatch-this&env=VITE_OPENAI_API_KEY&envDescription=Open%20AI%20API%20key&demo-title=watchthis.dev&demo-url=https%3A%2F%2Fwatchthis.dev"
+		href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2FB8N4S8N%2FN-C&env=OPENAI_API_KEY,OMDB_API_KEY&envDescription=Go%20to%20Open%20AI.%20Sign%20up.%20Get%20your%20API%20key.%20%20Enter%20the%20API%20key%20in%20ENV%20Variables%20and%20LFG!!!%20%20%20After%20that%20head%20to%20https%3A%2F%2Fwww.omdbapi.com%2F%20sign%20up%20and%20follow%20the%20instructions%20to%20get%20your%20API%20key.%20Enter%20it%20and%20LFG!!!&envLink=https%3A%2F%2Fplatform.openai.com%2Faccount%2Fapi-keys"
 	>
 		<span class="button_prefix__e8xDo"
 			><svg height="16" viewBox="0 0 113 100" width="16" xmlns="http://www.w3.org/2000/svg"
