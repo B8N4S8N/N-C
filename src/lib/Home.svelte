7796216953f3f3c1c821a133590f535ba6192432
B8N4S8N <79737258+B8N4S8N@users.noreply.github.com>
2023-03-11 19:49:55 +0000

    update

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -2,8 +2,8 @@
 	<div
 		class="text-center md:text-left font-bold text-slate-200 text-4xl md:text-5xl mb-8 md:mb-16 w-full"
 	>
-		Get curated show <br class="hidden md:block" /> and movie recommendations
-		<br class="hidden md:block" /> with Open AI
+		Self Care is Important <br class="hidden md:block" /> get curated TV and movie
+		<br class="hidden md:block" /> recommendations with Open AI <br class="hidden md:block" /> built with love by O.S.R.
 	</div>
 	<div class="flex flex-wrap items-center mr-auto">
 		<button
@@ -13,11 +13,11 @@
 			Get recommendations
 		</button>
 		<a
-			href="https://www.youtube.com/watch?v=Z3LrfdctOpU"
-			target="_blank"
+			href="https://guttermademedia.wixsite.com/opensourcerecovery"
+			target="_blank" rel="noreferrer"
 			class="w-full md:w-auto text-center bg-white/20 hover:bg-white/50 mb-4 flex-none rounded-full text-white font-medium py-3 px-6"
 		>
-			Learn how it's built
+			Visit Open Source Recovery
 		</a>
 		<script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="OSRecovery" data-description="Support me on Buy me a coffee!" data-message="If you want to donate to the cause, it helps us build a new future for addiction resources" data-color="#BD5FFF" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
 	</div>
